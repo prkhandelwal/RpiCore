a91b05bbcdf821c773989588d0b2b68b41b5a757
Pratik <prkhandelwal@hotmail.com>
2019-03-29 21:39:49 +0530

    Added Modes

--- RpiCore/I2CHelper.cs
+++ RpiCore/I2CHelper.cs
@@ -19,26 +19,27 @@ namespace RpiCore
         /// Sends control signal to the specified slave Arduino and retrieves response bytes.
         /// </summary>
         /// <param name="I2C_Slave_Address">Slave Arduino's address</param>
+        /// <param name="ControlMode">Select specific control mode</param>
         /// <param name="Pin">Pin to be set. ONLY VALID FOR MODE-2</param>
         /// <param name="PinValue">Value to be set.</param>
         /// <returns>Returns fourteen response byte.</returns>
-        public IAsyncOperation<IEnumerable<byte>> WriteRead(int I2C_Slave_Address, byte Pin, byte PinValue)
+        public IAsyncOperation<IEnumerable<byte>> WriteRead(int I2C_Slave_Address, byte Mode, byte Pin, byte PinValue)
         {
-            return this.WriteReadHelper(I2C_Slave_Address, Pin, PinValue).AsAsyncOperation();
+            return this.WriteReadHelper(I2C_Slave_Address, Mode, Pin, PinValue).AsAsyncOperation();
         }
 
         /// <summary>
-        /// Might use this one for reading from slave
+        /// Reads form specifird slave Arduino.
         /// </summary>
         /// <param name="I2C_Slave_Address">Slave Arduino's address</param>
         /// <returns>Returns fourteen response byte.</returns>
-        public IAsyncOperation<IEnumerable<byte>> WriteRead(int I2C_Slave_Address)
+        public IAsyncOperation<IEnumerable<byte>> Read(int I2C_Slave_Address)
         {
             return this.WriteReadHelper(I2C_Slave_Address).AsAsyncOperation();
         }
 
 
-        private async Task<IEnumerable<byte>> WriteReadHelper(int I2C_Slave_Address, byte Pin = 0, byte PinValue = 0)
+        private async Task<IEnumerable<byte>> WriteReadHelper(int I2C_Slave_Address, byte Pin = 0, byte Mode = 1, byte PinValue = 0)
         {
             while (Lock != false)
             {
@@ -64,7 +65,7 @@ namespace RpiCore
 
                 using (I2cDevice Device = await I2cDevice.FromIdAsync(DIS[0].Id, Settings))
                 {
-                    Device.Write(new byte[] { Pin, PinValue });
+                    Device.Write(new byte[] { Mode, Pin, PinValue });
 
                     Device.Read(res);
 
@@ -78,6 +79,49 @@ namespace RpiCore
 
             Lock = false;
             return Response;
+        }
+
+    }
+
+    public sealed class Mode
+    {
+        private static byte _RecieveSensorData = 0;
+
+        private static byte _RecieveDeviceState = 1;
+
+        private static byte _SendIOSignal = 2;
+
+        /// <summary>
+        /// Retrieves sensor data from specified I2C slave Arduino
+        /// </summary>
+        public byte ReceiveSensorData
+        {
+            get
+            {
+                return _RecieveSensorData;
+            }
+        }
+
+        /// <summary>
+        /// Retrieves devices state from specified I2C slave Arduino
+        /// </summary>
+        public byte RecieveDeviceState
+        {
+            get
+            {
+                return _RecieveDeviceState;
+            }
+        }
+
+        /// <summary>
+        /// Sends IO signal to pin of specified I2C slave Arduino
+        /// </summary>
+        public byte SendIOSignal
+        {
+            get
+            {
+                return _SendIOSignal;
+            }
         }
 
     }
